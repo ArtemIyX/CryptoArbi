bf4d39de5d9b3be4f11600f2b74245a1be08db15
SystemX <artem.podorozhko@outlook.com>
2024-04-19 22:11:04 +0300

    Arbi db context

---
+++ ArbiWriter/Data/ArbiDbContext.cs
@@ -0,0 +1,16 @@
+﻿
+
+using ArbiWriter.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ArbiWriter.Data
+{
+    public class ArbiDbContext : DbContext
+    {
+        public ArbiDbContext(DbContextOptions<ArbiDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<TestModel> YourEntities { get; set; }
+    }
+}
