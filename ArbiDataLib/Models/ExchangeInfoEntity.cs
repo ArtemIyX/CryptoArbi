e9d833a3f68432bb88a904ca84e08d6f40f0b46d
SystemX <artem.podorozhko@outlook.com>
2024-04-23 22:42:17 +0300

    Exchange info entity

---
+++ ArbiDataLib/Models/ExchangeInfoEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArbiDataLib.Models
+{
+    public class ExchangeInfoEntity
+    {
+        public string ExchangeId { get; set; } = string.Empty;
+        public string HomeUrl { get; set; } = string.Empty;
+        public string TradeUrl { get; set; } = string.Empty;
+        public string DepositUrl { get; set; } = string.Empty;
+        public string WithdrawalUrl { get; set;} = string.Empty;
+    }
+}
